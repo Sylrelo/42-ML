23778fd12bb6de3bce59d898c921874891814f69
Lou LOPEZ <loutrelop@hotmail.fr>
2024-11-03 00:58:52 +0100

    AUgmentation

--- leaffliction/src/Augmentation.py
+++ leaffliction/src/Augmentation.py
@@ -1,19 +1,116 @@
 import argparse
 import os
 from pathlib import Path
+import shutil
+from PIL import Image, ImageOps, ImageFilter
+import numpy as np
+import random
+from Distribution import compute_classes
 from Leaffliction import init_project
+
+def flip_image(image):
+    return ImageOps.flip(image)
+
+
+def rotate_image(image):
+    angle = 45
+    return image.rotate(angle, expand=True)
+
+# fr: inclinaision (both horizontal and vertical axes are moving)
+def skew_image(image):
+    skew_factor=0.5
+    (width, height) = image.size
+    width_shift = abs(skew_factor) * width
+    new_width = width + int(round(width_shift))
+    return image.transform((new_width, height), Image.AFFINE, (1, skew_factor, -width_shift if skew_factor > 0 else 0, 0, 1, 0))
+
+
+# The matrix is represented as a tuple (a, b, c, d, e, f). For horizontal shearing, set b and e to the shear factor
+def shear_image(image):
+    shear_factor=0.2
+    return image.transform(image.size, Image.AFFINE, (1, shear_factor, 0, shear_factor, 1, 0))
+
+def scale_image(image):
+    ratio = 0.6
+    width, height = image.size
+    new_width, new_height = int(width * ratio), int(height * ratio)
+    left = (width - new_width) // 2
+    top = (height - new_height) // 2
+    right = left + new_width
+    bottom = top + new_height
+    return image.crop((left, top, right, bottom)).resize((width, height))
+
+def blur_image(image):
+    return image.filter(ImageFilter.BLUR)
+
+
+def load_img(img_path):
+    image = Image.open(img_path)
+    return image
+
+def save_img(image, path, img_name, suffix):
+    image.save(Path(path, img_name + "_" + suffix + ".JPG"))
+
+
+AUGMENTATION_FUNC = {
+    "flip": flip_image,
+    "rotate": rotate_image,
+    "skew": skew_image,
+    "shear": shear_image,
+    "scale": scale_image,
+    "blur": blur_image
+}
+
+def augmentation(img_path, dest):
+    img = load_img(img_path)
+    img_name = Path(img_path).stem
+    for key in AUGMENTATION_FUNC:
+        save_img(AUGMENTATION_FUNC[key](img), dest, img_name, key)
+
+def copy_directory(source, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)  
+    shutil.copytree(source, dest)
+
+
+def balance(source, dest):
+    copy_directory(source, dest)
+    _, labels, img_counts = compute_classes(source)
+    max_val = max(img_counts)
+    print(f"max val is {max_val}")
+    for index, label in enumerate(labels):
+        count = img_counts[index]
+        print(f"{index} val is {count}")
+        files =  list((Path(source) / label).glob("*.JPG"))
+        while count < max_val:
+            file = random.choice(files)
+            files.remove(file)
+            augmentation(file, Path(dest) / label)
+            count += 6
+        print(f"{index} val is NOW {count}")
 
 
 if __name__ == '__main__':
     init_project()
     parser = argparse.ArgumentParser(
         description='Create variation of an image to complete the dataset')
-    parser.add_argument('img_path', help="the image path")
+    parser.add_argument('path', help="the image path or the base directory path if balanced")
+    parser.add_argument('--dest', help="the images destination", nargs='?', default="")
+    parser.add_argument('--balance', action=argparse.BooleanOptionalAction)
 
     args = parser.parse_args()
-    path = Path(args.img_path)
-    assert (os.path.exists(args.img_path)), f"Couln't find the image at\
-{args.img_path}"
-    assert (os.path.isfile(args.img_path) and
-            Path(args.img_path).suffix == ".JPG"), \
+    if args.balance :
+        assert args.dest != "", "You must specify a destination directory to create a balanced dataset"
+        assert (os.path.exists(args.path)), f"Couln't find the {args.path} directory"
+        balance(args.path, args.dest)
+    else:
+        assert (os.path.exists(args.path)), f"Couln't find the image at\
+{args.path}"
+        assert (os.path.isfile(args.path) and
+            Path(args.path).suffix == ".JPG"), \
         "providen path is not a JPG file"
+        if args.dest != "":
+            Path(args.dest).mkdir(parents=True, exist_ok=True)
+        dest = args.dest if args.dest != "" else os.path.dirname(args.path)
+        augmentation(args.path, dest)
+
